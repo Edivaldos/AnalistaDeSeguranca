39d5c051c5b99ee7e5bee49bb49daf44cd5b1f5f
Ed. Andrade <31553112+Edivaldos@users.noreply.github.com>
2025-01-06 17:49:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Exemplo: Adicionar um botão para imprimir o currículo
 const printButton = document.createElement('button');
-printButton.textContent = '<b>Imprimir Currículo</b>';
+printButton.textContent = '<b>\Imprimir Currículo\</b>';
 printButton.addEventListener('click', () => {
     window.print();
 });
